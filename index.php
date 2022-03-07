b6b8d1d74a75e7d4512dfacf1d831ecb5b1dcd4c
kvalter <valter@ya.ru>
2022-03-07 14:47:33 +0300

    first commit

---
+++ index.php
@@ -0,0 +1,21 @@
+<?php
+include_once './vendor/autoload.php';
+
+try {
+    $tron = new \IEXBase\TronAPI\Tron();
+
+    $generateAddress = $tron->generateAddress(); // or createAddress()
+    $isValid = $tron->isAddress($generateAddress->getAddress());
+
+
+    echo 'Address hex: '. $generateAddress->getAddress().PHP_EOL;
+    echo 'Address base58: '. $generateAddress->getAddress(true).PHP_EOL;
+    echo 'Private key: '. $generateAddress->getPrivateKey().PHP_EOL;
+    echo 'Public key: '. $generateAddress->getPublicKey().PHP_EOL;
+    echo 'Is Validate: '. $isValid;
+
+    echo 'Raw data: '.$generateAddress->getRawData();
+
+} catch (\IEXBase\TronAPI\Exception\TronException $e) {
+    echo $e->getMessage();
+}
