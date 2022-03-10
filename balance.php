f8731db07c07d396261ff8c5852604e8c11dea98
Serge Goodway <goodwaysarts@gmail.com>
2022-03-10 18:00:56 +0300

    check contract balance

--- balance.php
+++ balance.php
@@ -11,7 +11,7 @@ try {
     exit($e->getMessage());
 }
 
-
-$tron->setAddress('TWPmAxJE4sc1jauUa8WrKHzVod9uUojF2S');
-$balance = $tron->getBalance(null, true);
-var_dump($balance);
+$tron->setAddress('TTmypuYK8iEGX4dg4xiWRxcFJYoWBbRXQJ');
+$contract = $tron->contract('TR7NHqjeKQxGTCi8q8ZY4pL8otSzgjLj6t');
+$cBalance = $contract->balanceOf($tron->getAddress()['base58']);
+var_dump($cBalance);
