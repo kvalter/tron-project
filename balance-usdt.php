76b5b9aa0db4a010b607ce475759e06644bf7572
kvalter <konstantin.valter@gmail.com>
2022-04-17 16:36:36 +0300

    new balance and generate added

---
+++ balance-usdt.php
@@ -0,0 +1,17 @@
+<?php
+include_once './vendor/autoload.php';
+
+$fullNode = new \IEXBase\TronAPI\Provider\HttpProvider('https://api.trongrid.io');
+$solidityNode = new \IEXBase\TronAPI\Provider\HttpProvider('https://api.trongrid.io');
+$eventServer = new \IEXBase\TronAPI\Provider\HttpProvider('https://api.trongrid.io');
+
+try {
+    $tron = new \IEXBase\TronAPI\Tron($fullNode, $solidityNode, $eventServer);
+} catch (\IEXBase\TronAPI\Exception\TronException $e) {
+    exit($e->getMessage());
+}
+
+$tron->setAddress('TNejb4QiC2crc5t7pCdFczquEbnK1uUXmC');
+$contract = $tron->contract('TR7NHqjeKQxGTCi8q8ZY4pL8otSzgjLj6t');
+$cBalance = $contract->balanceOf($tron->getAddress()['base58']);
+var_dump($cBalance);
